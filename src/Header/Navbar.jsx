0fae3cf1a11b70029d5889389f2b9ec98f442b0e
Mohammadreza <mellboybeatprod@gmail.com>
2025-11-21 08:25:32 +0000

    حل کانفلیکت‌ها

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -4,12 +4,12 @@ export default function NavBar({darkmode}){
   return(
         <nav >
           <div className="flex justify-around md:justify-center items-center gap-x-1 md:gap-x-4 w-max">
-          <Link to="/" className={`flex items-center ${darkmode ?" dark:bg-gray-700  dark:text-gray-200 " : " bg-gray-400 text-gray-800 " }  px-2 md:px-4 py-2 border-gray-800 rounded-sm   text-sm md:text-lg lg:text-2xl hover:cursor-pointer list-none`}>خانه</Link>
+          <Link to="/" className={`flex items-center ${darkmode ?" dark:bg-gray-700  dark:text-gray-200 " : " bg-gray-400 text-gray-800 " }  px-2 md:px-4    lg:w-42  justify-center px-1  py-2 border-gray-800 rounded-sm   text-[10px] md:text-lg lg:text-2xl hover:cursor-pointer list-none`}>خانه</Link>
 
-          <Link to="project" className={`flex items-center ${darkmode ?" dark:bg-gray-700  dark:text-gray-200 " : " bg-gray-400 text-gray-800 " }  px-2 md:px-4 py-2 border-gray-800 rounded-sm   text-sm md:text-lg lg:text-2xl hover:cursor-pointer list-none`}>پروژه</Link>
+          <Link to="project" className={`flex items-center ${darkmode ?" dark:bg-gray-700  dark:text-gray-200 " : " bg-gray-400 text-gray-800 " }   lg:w-42    justify-center px-1  md:px-2 lg:px-4 py-2 border-gray-800 rounded-sm   text-[10px] md:text-lg lg:text-2xl hover:cursor-pointer list-none`}>پروژه</Link>
           
-          <Link to="/login" className={`flex items-center ${darkmode ?" dark:bg-gray-700  dark:text-gray-200 " : " bg-gray-400 text-gray-800 " }  px-2 md:px-4 py-2 border-gray-800 rounded-sm   text-sm md:text-lg lg:text-2xl hover:cursor-pointer list-none`}>ورود/ثبت نام</Link>
-          <Link to="/anjoman" className={`flex items-center ${darkmode ?" dark:bg-gray-700  dark:text-gray-200 " : " bg-gray-400 text-gray-800 " }  px-2 md:px-4 py-2 border-gray-800 rounded-sm   text-sm md:text-lg lg:text-2xl hover:cursor-pointer list-none`}>انجمن</Link>
+          <Link to="/login" className={`flex items-center ${darkmode ?" dark:bg-gray-700  dark:text-gray-200 " : " bg-gray-400 text-gray-800 " }   lg:w-42    justify-center  px-1 md:px-2 lg:px-4 py-2 border-gray-800 rounded-sm   text-[10px] md:text-lg lg:text-2xl hover:cursor-pointer list-none`}>ورود/ثبت نام</Link>
+          <Link to="/anjoman" className={`flex items-center ${darkmode ?" dark:bg-gray-700  dark:text-gray-200 " : " bg-gray-400 text-gray-800 " }    lg:w-42   justify-center px-1  md:px-2 lg:px-4 py-2 border-gray-800 rounded-sm   text-[10px] md:text-lg lg:text-2xl hover:cursor-pointer list-none`}>انجمن</Link>
           
 
           </div>
